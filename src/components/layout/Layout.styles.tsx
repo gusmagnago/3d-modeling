79b93210e2c1d9559d8a66a90de77e36dec62335
Gustavo Magnago <ext.gustavo.magnago@farfetch.com>
2023-06-09 13:41:02 +0300

    feat: some generic navigation

--- src/components/layout/Layout.styles.tsx
+++ src/components/layout/Layout.styles.tsx
@@ -11,3 +11,22 @@ export const ContentWrapper = styled.div`
   height: 100%;
   width: 100%;
 `;
+
+export const NavigationWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-width: 200px;
+  height: 100%;
+  align-items: center;
+  margin-top: 50px;
+`;
+
+export const Link = styled.div`
+  margin-bottom: 20px;
+  & a {
+    text-decoration: none;
+    color: #000000;
+    text-transform: uppercase;
+    font-size: 20px;
+  }
+`;
